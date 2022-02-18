154580c0721e4ab78479a53fb4ed66fea0e59819
BuildTools <unconfigured@null.spigotmc.org>
2022-02-17 20:10:10 -0800

    Deleted comment testing commits working

---
+++ src/main/java/net/zack7796/tutorialmod/TutorialMod.java
@@ -0,0 +1,19 @@
+package net.zack7796.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
