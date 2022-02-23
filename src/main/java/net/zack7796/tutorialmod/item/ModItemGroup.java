69d82a7f9e6875dda064cfbc44d62825092b6de6
BuildTools <unconfigured@null.spigotmc.org>
2022-02-23 13:00:24 -0800

    Added brick pillar

---
+++ src/main/java/net/zack7796/tutorialmod/item/ModItemGroup.java
@@ -0,0 +1,13 @@
+package net.zack7796.tutorialmod.item;
+
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.zack7796.tutorialmod.TutorialMod;
+
+public class ModItemGroup {
+    public static final ItemGroup MYTHRIL = FabricItemGroupBuilder.build(new Identifier(TutorialMod.MOD_ID, "mythril"),
+            () -> new ItemStack(ModItems.MYTHRIL_INGOT));
+}
