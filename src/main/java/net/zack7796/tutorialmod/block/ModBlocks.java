624daa5c9832006f070d17417c2ba3b5a5d6b538
BuildTools <unconfigured@null.spigotmc.org>
2022-03-06 15:22:16 -0800

    Add Amethyst Stairs and Slabs, Calcite Bricks, and fixed block drops for Calcite Stairs

--- src/main/java/net/zack7796/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/zack7796/tutorialmod/block/ModBlocks.java
@@ -8,10 +8,12 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Matrix3f;
 import net.zack7796.tutorialmod.TutorialMod;
 import net.minecraft.util.registry.Registry;
+import net.zack7796.tutorialmod.block.custom.ModLampBlock;
 import net.zack7796.tutorialmod.block.custom.ModStairsBlock;
 import net.zack7796.tutorialmod.item.ModItemGroup;
 
@@ -39,14 +41,23 @@ public class ModBlocks {
     //Light Blocks
     public static final Block JEDI_HOLOCRON = registerBlock("jedi_holocron",
             new Block(FabricBlockSettings.of(Material.GLASS).strength(1F).breakByHand(true).sounds(BlockSoundGroup.GLASS).luminance((int) 16F)), ModItemGroup.MYTHRIL);
+    public static final Block STRONGHOLD_LAMP = registerBlock("stronghold_lamp",
+            new ModLampBlock(FabricBlockSettings.of(Material.STONE)
+                    .strength(1.5f).requiresTool()
+                    .luminance((state) -> state.get(ModLampBlock.CLICKED) ? 15 : 0)
+                    .sounds(BlockSoundGroup.STONE)), ModItemGroup.MYTHRIL);
 
     //Slab Blocks
     public static final Block CALCITE_SLAB = registerBlock("calcite_slab",
             new SlabBlock(FabricBlockSettings.of(Material.STONE).strength(0.8F).requiresTool().sounds(BlockSoundGroup.CALCITE)), ModItemGroup.MYTHRIL);
+    public static final Block AMETHYST_SLAB = registerBlock("amethyst_slab",
+            new SlabBlock(FabricBlockSettings.of(Material.STONE).strength(1.5F).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)), ModItemGroup.MYTHRIL);
 
     //Stair Blocks
     public static final Block CALCITE_STAIRS = registerBlock("calcite_stairs",
             new ModStairsBlock(ModBlocks.CALCITE_SLAB.getDefaultState(),FabricBlockSettings.of(Material.STONE).strength(0.8F).requiresTool().sounds(BlockSoundGroup.CALCITE)), ModItemGroup.MYTHRIL);
+    public static final Block AMETHYST_STAIRS = registerBlock("amethyst_stairs",
+            new ModStairsBlock(ModBlocks.AMETHYST_SLAB.getDefaultState(),FabricBlockSettings.of(Material.STONE).strength(1.5F).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)), ModItemGroup.MYTHRIL);
 
 
 
